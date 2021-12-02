0defb326816930e584f63f6914362dc834d2934e
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2021-12-02 09:22:27 -0300

    add rating stars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
+import { StarComponent } from './star/star.component';
 
 @NgModule({
-  declarations: [AppComponent, CourseListComponent],
+  declarations: [AppComponent, CourseListComponent, StarComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
